7ea8179cf952540a96c141f878a39348312b21b8
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-18 21:05:58 -0300

    ok

--- conn.js
+++ conn.js
@@ -1,3 +1,4 @@
+const conn = require('./conn');
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize('nodesequelize', 'root', '', {
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const conn = require('./conn');
 require('dotenv').config();
 const express = require('express')
 const app = express()
 
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World 2023!</h1>');
+  res.send(`<h1>Hello World 2023! na porta ${process.env.PORT}</h1>`);
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening in ${process.env.PORT}`)
-})
+//app.listen(process.env.PORT, () => {
+  //console.log(`Example app listening in ${process.env.PORT}`)
+//});
+
+conn.sync().then(() => {
+    app.listen(process.env.PORT)
+  })
+  .catch((err) => console.log(err))
